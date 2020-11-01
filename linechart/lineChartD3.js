f363632c7725bbac8b15f7282961cc83d3986dd0
mylee1995 <mylee1995@gmail.com>
2020-11-01 00:35:24 -0400

    stack arr kinda working

--- linechart/lineChartD3.js
+++ linechart/lineChartD3.js
@@ -31,9 +31,71 @@ export default function lineChartD3(container) {
   let yAxisGroup = group.append('g').attr('class', 'y-axis axis');
 
   function update(data, keys) {
-    stackProcessing(data);
+    colorScale.domain(keys);
 
-    console.log('end of lineD3 update function');
+    console.log(keys);
+
+    const obj = {};
+    data.forEach((e) => {
+      const year = e['funded_year'];
+      const region = e['company_region'];
+      if (keys.includes(region)) {
+        // console.log('he');
+        if (!obj[year]) {
+          obj[year] = {};
+        }
+        if (obj[year][region] !== undefined) {
+          obj[year][region] += e['raised_amount_usd'];
+        } else {
+          obj[year][region] = 0;
+        }
+      }
+    });
+
+    
+    const objKeys = Object.keys(obj);
+    objKeys.forEach((key) => {
+      obj[key]['year'] = parseInt(key);
+    });
+
+    const arr = [];
+    objKeys.forEach((key) => {
+      arr.push(obj[key]);
+    });
+
+    const stack = d3
+      .stack()
+      .keys(keys)
+      .value((datum, key) => {
+        if (isNaN(datum[key])) {
+          return 0;
+        }
+        return datum[key];
+      })
+      .order(d3.stackOrderNone)
+      .offset(d3.stackOffsetNone);
+
+    const series = stack(arr);
+
+    console.log(series);
+
+    const area = d3
+      .area()
+      .x((d) => xScale(d.data.year))
+      .y0((d) => yScale(d[0]))
+      .y1((d) => yScale(d[1]));
+
+    const areas = group.selectAll('.area').data(series, (d) => d.key);
+
+    areas
+      .enter()
+      .append('path')
+      .attr('class', 'area')
+      .merge(areas)
+      .attr('d', area)
+      .attr('fill', (d) => 'steelblue');
+
+    areas.exit().remove();
   }
 
   return {
