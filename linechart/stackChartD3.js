6bdbe0d0da4d3812630ca698d1a487f8a8441d5b
mylee1995 <mylee1995@gmail.com>
2020-11-01 20:26:46 -0500

    rect clicking update as well

--- linechart/stackChartD3.js
+++ linechart/stackChartD3.js
@@ -1,11 +1,11 @@
-import stackProcessing from './stackProcessing.js';
+import dataProcessor from './dataProcessor.js';
 
 export default function lineChartD3(container) {
   ///////// Initialization //////////
   // Create a SVG with the margin convention
-  const margin = { top: 20, right: 20, bottom: 20, left: 100 };
+  const margin = { top: 20, right: 200, bottom: 20, left: 100 };
   const width = 1000 - margin.left - margin.right;
-  const height = 1000 - margin.top - margin.bottom;
+  const height = 800 - margin.top - margin.bottom;
 
   const svg = d3
     .select(container)
@@ -27,7 +27,12 @@ export default function lineChartD3(container) {
   const xAxis = d3.axisBottom().scale(xScale).ticks(10);
   let xAxisGroup = group.append('g').attr('class', 'x-axis axis');
 
-  const yAxis = d3.axisLeft().scale(yScale);
+  const yAxis = d3
+    .axisLeft()
+    .scale(yScale)
+    .tickFormat(function (d) {
+      return d / 1000000000 + ' Billion';
+    });
   let yAxisGroup = group.append('g').attr('class', 'y-axis axis');
 
   // Create a category label (tooltip)
@@ -63,25 +68,31 @@ export default function lineChartD3(container) {
     if (event.selection) {
       const inverted = event.selection.map(xScale.invert);
       xDomain = inverted;
-      update(_data, _keys);
+      update(_data, _keys, _category);
     } else {
       xDomain = [new Date(2004, 11), new Date(2014, 1)];
-      update(_data, _keys);
+      update(_data, _keys, _category);
     }
   }
 
   let _data;
-  let _keys;
+  let _category;
   let xDomain;
+  let _keys;
 
-  function update(data, keys) {
+  function update(data, keys, category) {
     if (xDomain) {
       group.select('.brush').call(brush.move, null);
     }
     _data = data;
-    _keys = keys;
+    if (keys.length > 1) _keys = keys;
+    _category = category;
     // Process data into D3 stack format
-    const stackProcessedData = stackProcessing(data, keys);
+    const stackProcessedData = dataProcessor.stackProcessing(
+      data,
+      keys,
+      category
+    );
 
     // Create stack layout (Ascending order)
     const stack = d3
@@ -98,11 +109,13 @@ export default function lineChartD3(container) {
 
     // Call layout
     const series = stack(stackProcessedData);
+    const max = d3.max(series[0], (d) => d[1] - d[0]);
+    console.log('max', max);
 
     // Set domain for xScale, yScale and colorScale
     xScale.domain(xDomain ? xDomain : [new Date(2004, 11), new Date(2014, 1)]);
-    yScale.domain([0, 12000000000]);
-    colorScale.domain(keys);
+    yScale.domain([0, max]);
+    colorScale.domain(_keys);
 
     const area = d3
       .area()
@@ -117,8 +130,8 @@ export default function lineChartD3(container) {
       .append('path')
       .style('clip-path', 'url(#clip)')
       .attr('class', (d) => {
-        const replaced = d.key.replace(/\s+/g, '-')
-        return 'area ' + replaced
+        const replaced = d.key.replace(/\s+/g, '-');
+        return 'area ' + replaced;
       })
       .merge(areas)
       .attr('fill', (d) => colorScale(d.key))
@@ -128,11 +141,18 @@ export default function lineChartD3(container) {
       .on('mouseout', () => {
         tooltip.text('');
       })
+      .on('click', (e, d) => {
+        if (keys.length === 1) {
+          update(data, _keys, category);
+        } else {
+          update(data, [d.key], category);
+        }
+      })
       .transition()
       .duration(1000)
       .attr('d', area);
 
-    areas.exit().transition().duration(1000).remove();
+    areas.exit().remove();
 
     // Update axes
     xAxisGroup
@@ -147,9 +167,8 @@ export default function lineChartD3(container) {
     //////////////
 
     const highlight = function (d) {
-      console.log('highlight', d);
       group.selectAll('.area').style('opacity', 0.2);
-      const replaced = d.replace(/\s+/g, '-')
+      const replaced = d.replace(/\s+/g, '-');
       group.select('.' + replaced).style('opacity', 1);
     };
 
@@ -157,36 +176,51 @@ export default function lineChartD3(container) {
       group.selectAll('.area').style('opacity', 1);
     };
 
-    const rectSize = 20;
+    const rectSize = 30;
+    const legendX = width + 50;
+    const legendY = height - keys.length * 40;
 
-    group
-      .selectAll('.legend')
-      .data(keys)
+    const rects = group.selectAll('.legend').data(keys, (d) => d);
+
+    rects
       .enter()
       .append('rect')
       .attr('class', 'legend')
-      .attr('x', 400)
-      .attr('y', (d, i) => 10 + i * (rectSize + 5))
+      .merge(rects)
+      .attr('x', legendX)
+      .attr('y', (d, i) => legendY + i * (rectSize + 5))
       .attr('width', rectSize)
       .attr('height', rectSize)
       .style('fill', (d) => colorScale(d))
       .on('mouseover', (e, d) => {
         highlight(d);
-        console.log('YOYOYO legend');
       })
-      .on('mouseleave', noHighlight);
+      .on('mouseleave', noHighlight)
+      .on('click', (e, d) => {
+        if (keys.length === 1) {
+          update(data, _keys, category);
+        } else {
+          update(data, [d], category);
+        }
+      });
 
-    group
-      .selectAll('labels')
-      .data(keys)
+    rects.exit().remove();
+
+    const labels = group.selectAll('.labels').data(keys, (d) => d);
+
+    labels
       .enter()
       .append('text')
-      .attr('x', 400 + rectSize * 1.2)
-      .attr('y', (d, i) => 10 + i * (rectSize + 5) + rectSize / 2)
+      .attr('class', 'labels')
+      .merge(labels)
+      .attr('x', legendX + rectSize * 1.2)
+      .attr('y', (d, i) => legendY + i * (rectSize + 5) + rectSize / 2)
       .style('fill', (d) => colorScale(d))
       .text((d) => d)
       .attr('text-anchor', 'left')
       .style('alignment-baseline', 'middle');
+
+    labels.exit().remove();
   }
 
   return {
